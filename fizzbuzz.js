17f4855db06211f520ce44a523445013a4acf4d0
SooHyun-Kang <k.josephine19@gmail.com>
2023-07-03 14:36:52 -0700

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,33 @@
+/** My solution **/
+function fizzBuzz(n) {
+  let index = 1;
+  while (index <= n) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (index % 3 === 0) {
+      console.log('fizz');
+    } else if (index % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(index);
+    }
+    index++;
+  }
+}
+
+/** Instructor's solution **/
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(15);
